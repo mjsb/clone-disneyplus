6e8f416b605c9af089af7071889e8fc87b545e13
mjsb <mjsb@github.com>
2025-03-27 11:52:24 -0300

    Add js minification

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,19 @@
 const gulp = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
 const imagemin = require('gulp-imagemin')
+const uglify = require('gulp-uglify')
 
 // function testeGulp(cb) {
 //     console.log("Hello world!");
 //     cb();
 // }
+
+function scripts() {
+    return gulp
+        .src('./src/scripts/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/js'))
+}
 
 function styles() {
     return gulp
@@ -21,7 +29,8 @@ function images() {
         .pipe(gulp.dest('./dist/images'))
 }
 
-exports.default = gulp.parallel(styles, images)
+exports.default = gulp.parallel(styles, images, scripts)
 exports.watch = function () {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
+    gulp.watch('./src/scripts/*.js', gulp.parallel(scripts))
 }
