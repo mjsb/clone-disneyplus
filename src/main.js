9543f52a5dd7c9b1d0cff69780f1cf4d1c4f666f
mjsb <mjsb@github.com>
2025-03-27 10:29:27 -0300

    Adjusts for mobile

--- src/main.js
+++ src/main.js
@@ -1,5 +1,21 @@
 document.addEventListener('DOMContentLoaded', function () {
     const buttons = document.querySelectorAll('[data-tab-button]')
+    const questions = document.querySelectorAll('[data-faq-question]')
+
+    const heroSection = document.querySelector('.hero')
+    const alturaHero = heroSection.clientHeight
+
+    window.addEventListener('scroll', function() {
+        const posicaoAtual = window.scrollY;
+
+        if ( posicaoAtual < alturaHero ) {
+            ocultaHeader()
+        } else {
+            exibeHeader()
+        }
+    })
+
+    // Seção de atrações, programação das abas.
 
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function (button) {
@@ -11,7 +27,30 @@ document.addEventListener('DOMContentLoaded', function () {
             button.target.classList.add('shows__tabs__button--is-active')
         })
     }
+
+    // Seção FAQ, acordion
+
+    for (let i = 0; i < questions.length; i++) {
+        questions[i].addEventListener('click', abreFechaResposta)
+    }
 })
+
+function ocultaHeader() {
+    const header = document.querySelector('header')
+    header.classList.add('header--is-hidden')
+}
+
+function exibeHeader() {
+    const header = document.querySelector('header')
+    header.classList.remove('header--is-hidden')
+}
+
+function abreFechaResposta(elemento) {
+    const classe = 'faq__questions__item--is-open'
+    const elementoPai = elemento.target.parentNode
+
+    elementoPai.classList.toggle(classe)
+}
 
 function removeActiveButton() {
     const buttons = document.querySelectorAll('[data-tab-button]')
